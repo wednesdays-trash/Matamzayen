c7420ed8e9b33f429d8ce2361691dfea886f2aab
itswednesday <qoev@protonmail.com>
2018-02-05 20:47:48 +0200

    Added funcs: half, negate, zero

--- src/Matamzayen/BaseFuncs.hs
+++ src/Matamzayen/BaseFuncs.hs
@@ -3,8 +3,11 @@ module Matamzayen.BaseFuncs where
 getFuncByString :: Floating a => String -> [a -> a] -> (a -> a)
 getFuncByString str userFuncs =
   case str of
-    "inc" -> (+ 1)
-    "dec" -> subtract 1
+    "inc"    -> (+ 1)
+    "dec"    -> subtract 1
     "double" -> (* 2)
-    "sqrt" -> sqrt
-    n -> userFuncs !! read n
+    "half"   -> (/ 2)
+    "sqrt"   -> sqrt
+    "negate" -> negate
+    "zero"   -> const 0
+    n        -> userFuncs !! read n
