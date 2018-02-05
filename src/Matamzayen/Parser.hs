c93ba232d6a159b9afd61ca7d349525e3776298c
itswednesday <qoev@protonmail.com>
2018-02-05 17:53:03 +0200

    support for user defined functions

--- src/Matamzayen/Parser.hs
+++ src/Matamzayen/Parser.hs
@@ -7,25 +7,31 @@ mainFuncIdentifier = '@'
 funcSeparator = "."
 funcIdentifier = "^"
 
-funcFromString :: Floating a => String -> (a -> a)
-funcFromString str =
-    case str of
-        "inc"    -> (+ 1)
-        "dec"    -> subtract 1
-        "double" -> (* 2)
-        "sqrt"   -> sqrt
-
-createComposition :: Floating a => String -> (a -> a)
-createComposition = foldr1 (.) . map funcFromString . splitOn funcSeparator
-
-getMainFunc :: Floating a => [String] -> (a -> a)
-getMainFunc funcsStrings =
-    let str = tail . head . filter (elem mainFuncIdentifier) $ funcsStrings
-    in createComposition str
-
 parse :: Floating a => String -> a -> a
 parse program = getMainFunc funcsStrings
   where
-    funcsStrings = splitOn funcIdentifier (tail program)
-    funcsStringWithoutMain = filter (notElem mainFuncIdentifier) funcsStrings
-    funcs = map createComposition funcsStringWithoutMain
+    funcsStrings :: [String]
+    funcsStrings = tail $ splitOn funcIdentifier program
+
+    funcsStringsWithoutMain :: [String]
+    funcsStringsWithoutMain = filter (notElem mainFuncIdentifier) funcsStrings
+
+    funcs :: Floating a => [a -> a]
+    funcs = map createComposition funcsStringsWithoutMain
+
+    funcFromString :: Floating a => String -> (a -> a)
+    funcFromString str =
+        case str of
+            "inc"    -> (+ 1)
+            "dec"    -> subtract 1
+            "double" -> (* 2)
+            "sqrt"   -> sqrt
+            n        -> funcs !! read n
+    
+    createComposition :: Floating a => String -> (a -> a)
+    createComposition = foldr1 (.) . map funcFromString . splitOn funcSeparator
+    
+    getMainFunc :: Floating a => [String] -> (a -> a)
+    getMainFunc funcsStrings =
+        let str = tail . head . filter (elem mainFuncIdentifier) $ funcsStrings
+        in createComposition str
