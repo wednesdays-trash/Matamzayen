f08fa70cb76ab2e6cae04c252802453f532f3e8f
itswednesday <qoev@protonmail.com>
2018-02-09 16:04:50 +0200

    Added tests

---
+++ test/Tests.hs
@@ -0,0 +1,34 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Matamzayen.Parser
+
+exampleProgram =
+  "^double.double.inc\n^inc.2\n  ^double.double #neat comment#    ^@0.1"
+
+main = defaultMain (testGroup "Parser tests" tests)
+  where
+    tests = [removeWhitespacesAndCommentsTest, parseBlockTest]
+
+removeWhitespacesAndCommentsTest =
+  testCase "Testing removeWhitespaces and removeComments" assertion
+  where
+    expectedResult = "^double.double.inc^inc.2^double.double^@0.1"
+    assertion =
+      assertEqual
+        "Should remove sapces, tabs, newlines and comments"
+        expectedResult
+        ((removeWhitespaces . removeComments) exampleProgram)
+
+parseBlockTest =
+  testCase "Testing actual program parsing and evaluation" assertion
+  where
+    input = 3
+    expectedResult = 56.0
+    assertion =
+      assertEqual
+        "Should evaluate the given program and apply its main function on the input"
+        expectedResult
+        (parseBlock exampleProgram input)
