e69cecc827ba04fda10a38cb17d16fc88801f30a
itswednesday <qoev@protonmail.com>
2018-02-26 15:23:04 +0200

    Moved function declarations into maps

--- src/Matamzayen/BaseFuncs.hs
+++ src/Matamzayen/BaseFuncs.hs
@@ -1,41 +1,51 @@
 module Matamzayen.BaseFuncs where
 
 import Control.Monad
+import Data.HashMap.Lazy
 import Data.List.Split
+import Data.Maybe
 import qualified Safe
 
 isCurried :: String -> Bool
 isCurried = elem ' '
 
+twoParamsFuncs :: HashMap String (Double -> Double -> Double)
+twoParamsFuncs =
+  fromList [("add", (+)), ("sub", (-)), ("div", (/)), ("mul", (*))]
+
 getCurriedFuncByString :: String -> (Double -> Double)
-getCurriedFuncByString str = func (read param)
+getCurriedFuncByString funcString = func (read param)
   where
-    s = splitOn " " str
+    s = splitOn " " funcString
     funcName = head s
     param = s `Safe.at` 1
     func =
-      case funcName of
-        "add" -> (+)
-        "sub" -> (-)
-        "div" -> (/)
-        "mul" -> (*)
+      fromMaybe
+        (\x y -> 5) -- temporary patch
+        (Data.HashMap.Lazy.lookup funcName twoParamsFuncs)
+
+funcs :: HashMap String (Double -> Double)
+funcs =
+  fromList
+    [ ("inc", (+ 1))
+    , ("dec", subtract 1)
+    , ("double", (* 2))
+    , ("half", (/ 2))
+    , ("sqrt", sqrt)
+    , ("negate", negate)
+    , ("zero", const 0)
+    , ("id", id)
+    , ("square", join (*))
+    , ("exp", exp)
+    , ("sin", sin)
+    , ("con", cos)
+    , ("tan", tan)
+    ]
 
 getFuncByString :: String -> [Double -> Double] -> (Double -> Double)
-getFuncByString str userFuncs =
-  if isCurried str
-    then getCurriedFuncByString str
-    else case str of
-           "inc" -> (+ 1)
-           "dec" -> subtract 1
-           "double" -> (* 2)
-           "half" -> (/ 2)
-           "sqrt" -> sqrt
-           "negate" -> negate
-           "zero" -> const 0
-           "id" -> id
-           "square" -> join (*)
-           "exp" -> exp
-           "sin" -> sin
-           "cos" -> cos
-           "tan" -> tan
-           n -> userFuncs `Safe.at` read n
+getFuncByString funcName userFuncs =
+  if isCurried funcName
+    then getCurriedFuncByString funcName
+    else fromMaybe
+           (userFuncs `Safe.at` read funcName)
+           (Data.HashMap.Lazy.lookup funcName funcs)
